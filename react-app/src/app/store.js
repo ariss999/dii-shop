dd40614fd99988196bce4b5bcc70f42ed4787c15
ariss999 <87415367+ariss999@users.noreply.github.com>
2023-10-15 09:55:26 +0700

    delete-react

--- react-app/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import productReducers from '../features/Product/reducers';
-
-export default configureStore({
-  reducer: {
-    products: productReducers
-  }
-});
